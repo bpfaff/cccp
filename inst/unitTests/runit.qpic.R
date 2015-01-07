bfb7aa07afcaf387267e4ba394011f816d0b4de5
Bernhard Pfaff <bernhard@pfaffikus.de>
2015-01-07 17:55:53 +0100

    Added further unit tests

---
+++ inst/unitTests/runit.qpic.R
@@ -0,0 +1,16 @@
+##
+## Unit testing of Quadratic Program with equality and inequality constraints
+test.QPIC <- function(){
+    P <- 2 * matrix(c(2, .5, .5, 1), nrow = 2, ncol = 2)
+    q <- c(1.0, 1.0)
+    G <- -diag(2)
+    h <- rep(0, 2)
+    nno1 <- nnoc(G = G, h = h)
+    A <- matrix(c(1.0, 1.0), nrow = 1, ncol = 2)
+    b <- 1.0
+    cpd <- dqp(P = P, q = q, A = A, b = b, cList = list(nno1))
+    ctl <- ctrl()
+    ans <- cpd$cps(ctl)
+    checkTrue(ans$status == "optimal")
+    return()
+}
